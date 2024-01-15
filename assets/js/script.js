1b24b19b98d47104a03ec60de6bd88ca9e5d3cf6
22kszov <>
2024-01-15 16:40:37 +0000

    Add division game mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,6 +51,10 @@ function runGame(gameType) {
 
         displaySubtractQuestion(num1, num2);
 
+    } else if (gameType === "division") {
+
+        displayDivisionQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -101,6 +105,10 @@ function calculateCorrectAnswer() {
 
         return [operand1 - operand2, "subtract"];
 
+    } else if (operator === "/") {
+
+        return [operand1 / operand2, "division"];
+
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -149,5 +157,16 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+
+}
+
+
+function displayDivisionQuestion(operand1, operand2) {
+
+
+    document.getElementById("operand1").textContent = operand2 % operand1 === 0 ? console.log("divisible numbers") : operand1 = operand1 * operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "/";
 
 }
