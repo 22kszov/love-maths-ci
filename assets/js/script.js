3b53e74634eab39fc82cb60b25913f193a43a122
22kszov <>
2024-01-15 15:42:39 +0000

    Add subtraction game mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,10 @@ function runGame(gameType) {
 
         displayMultiplyQuestion(num1, num2);
 
+    } else if (gameType === "subtract") {
+
+        displaySubtractQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -82,7 +86,11 @@ function calculateCorrectAnswer() {
 
     } else if(operator === "x") {
 
-        return [operand1 * operand2, "multiply"]
+        return [operand1 * operand2, "multiply"];
+
+    } else if(operator === "-") {
+
+        return[operand1 - operand2, "subtract"];
 
     } else {
         alert(`Unimplemented operator ${operator}`);
@@ -119,7 +127,11 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").innerText = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").innerText = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").innerText = "-"
 
 }
 
